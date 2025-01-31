de7d10c6e9f3beaba9ecb75c40b15e3a0baa280c
ardur77 <rtiwari2602@gamil.com>
2025-01-31 03:47:48 +0000

    Fix forge fmt issue

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.18;
 import {Script} from "forge-std/Script.sol";
 import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
 
-contract HelperConfig is Script{
+contract HelperConfig is Script {
     NetworkConfig public activeNetworkConfig;
 
     uint8 public constant DECIMALS = 8;
@@ -12,27 +12,24 @@ contract HelperConfig is Script{
 
     struct NetworkConfig {
         address priceFeed;
-        }
+    }
 
-        constructor(){
-            if (block.chainid == 11155111){
-                activeNetworkConfig = getSepoliaEthConfig();
-            } 
-            else if(block.chainid == 1){
-                activeNetworkConfig = getMainnetEthConfig();}
-                
-                    else{
-                        activeNetworkConfig = getAnvilEthConfig();
-            } 
-            
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else if (block.chainid == 1) {
+            activeNetworkConfig = getMainnetEthConfig();
+        } else {
+            activeNetworkConfig = getAnvilEthConfig();
         }
+    }
 
     function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
         NetworkConfig memory sepoliaConfig = NetworkConfig({priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306});
         return sepoliaConfig;
     }
 
-    function getMainnetEthConfig() public pure returns (NetworkConfig memory){
+    function getMainnetEthConfig() public pure returns (NetworkConfig memory) {
         NetworkConfig memory ethConfig = NetworkConfig({priceFeed: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419});
         return ethConfig;
     }
--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -10,39 +10,30 @@ contract FundFundMe is Script {
 
     function fundFundMe(address mostRecentlyDeployed) public {
         vm.startBroadcast();
-        FundMe(payable(mostRecentlyDeployed)).fund{value:SEND_VALUE}();
+        FundMe(payable(mostRecentlyDeployed)).fund{value: SEND_VALUE}();
         vm.stopBroadcast();
-        
-        
+
         console.log("Funded FundMe with %s", SEND_VALUE);
     }
 
-
-    function run() external{
-        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment
-        ("FundMe", block.chainid);
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("FundMe", block.chainid);
 
         vm.startBroadcast();
         fundFundMe(mostRecentlyDeployed);
         vm.stopBroadcast();
     }
-    
 }
 
-contract WithdrawFundMe is Script{
-
+contract WithdrawFundMe is Script {
     function withdrawFundMe(address mostRecentlyDeployed) public {
-
         vm.startBroadcast();
         FundMe(payable(mostRecentlyDeployed)).withdraw();
         vm.stopBroadcast();
-        
     }
 
-
-    function run() external{
-        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment
-        ("FundMe", block.chainid);
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("FundMe", block.chainid);
 
         vm.startBroadcast();
         withdrawFundMe(mostRecentlyDeployed);
